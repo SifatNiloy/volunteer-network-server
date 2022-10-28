391ea98414b527534b8ee3b11538663a32746f18
unknown <sifatniloy18@gmail.com>
2022-10-28 10:07:48 -0600

    delete api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -24,6 +24,29 @@ async function run() {
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
+        })
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+
+        })
+
+        //post
+        app.post('/service', async (req, res) => {
+
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+
+        })
+        //delete
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { id: ObjectId(id) };
+            const result = serviceCollection.deleteOne(query);
+            res.send(result);
         })
     }
     finally {
