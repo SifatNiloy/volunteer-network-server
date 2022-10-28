75d11ca8e99fd21d9c6b4413dbe32cdafb2b3e1c
unknown <sifatniloy18@gmail.com>
2022-10-27 19:24:53 -0600

    connected to mongodb

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,6 +8,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+//user : volunteer1
+//pass: N9aytHQ6lRzdLfeN
+
+
+const uri = "mongodb+srv://volunteer1:N9aytHQ6lRzdLfeN@cluster0.dxklnjh.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db("volunteers").collection("services");
+        const service = { name: 'animal shelter' };
+        const result = await serviceCollection.insertOne(service);
+        console.log(`user inserted with id ${result.insertedId}`)
+    }
+    finally {
+        // await client.close();
+    }
+
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
